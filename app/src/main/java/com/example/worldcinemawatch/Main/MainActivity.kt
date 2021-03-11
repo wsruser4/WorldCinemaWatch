843184637c87ce98cde9d127b3f7b8003e8de910
wsruser <adreyseminforws@yandex.ru>
2021-03-11 15:46:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/worldcinemawatch/Main/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.worldcinemawatch.Main
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import com.example.worldcinemawatch.R
+
+class MainActivity : WearableActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        // Enables Always-on
+        setAmbientEnabled()
+    }
+}
---
+++ app/src/main/java/com/example/worldcinemawatch/SgnIn/SignInScreen.kt
@@ -0,0 +1,12 @@
+package com.example.worldcinemawatch.SgnIn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.worldcinemawatch.R
+
+class SignInScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_screen)
+    }
+}
